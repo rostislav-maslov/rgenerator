b4aa905a52b54848c0082fb470dadffd6c304663
Rostislav Maslov <maslov.rostislav@gmail.com>
2020-03-28 07:21:48 +0400

    Test surf plugin

---
+++ backend/src/test/java/v1/base/TestSimple.java
@@ -0,0 +1,13 @@
+package v1.base;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestSimple {
+
+    @Test
+    public void test(){}
+
+}
